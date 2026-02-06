11955c3add93234af235d09170d62c0bf1fa1997
Labhesh <labhesh.dudi@gmail.com>
2026-02-06 16:18:36 +0530

    initial commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Labhesh Dudi on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
